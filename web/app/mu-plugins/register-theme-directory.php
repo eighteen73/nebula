488eacfecb9f1b8432fcc56c46dc9f18d5bcc996
Brett Mason <brettsmason@gmail.com>
2022-04-27 18:27:20 +0100

    Update namespace

--- web/app/mu-plugins/register-theme-directory.php
+++ web/app/mu-plugins/register-theme-directory.php
@@ -5,13 +5,12 @@
  * Description:  Register default theme directory
  * Version:      1.0.0
  * Author:       eighteen73
- * Author URI:   https://eighteen73.agency/
  * License:      MIT License
  *
  * @package Nebula
  */
 
-namespace eighteen73\Nebula;
+namespace Eighteen73\Nebula;
 
 if ( ! defined( 'WP_DEFAULT_THEME' ) ) {
 	register_theme_directory( ABSPATH . 'wp-content/themes' );
