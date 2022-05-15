08b9c4730ff410229ffadb73a0468a92c3d5d57d
Ed Jeavons <ed@orphans.co.uk>
2022-05-15 21:54:50 +0100

    Include and bootstrap Carbon Fields as a standard feature

---
+++ web/app/mu-plugins/nebula-carbon-fields.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name:  Nebula Carbon Fields
+ * Plugin URI:   https://github.com/eighteen73/nebula/
+ * Description:  Initialize Carbon Fields, so they're available for use by other plugins and themes.
+ * Version:      0.1.0
+ * Author:       eighteen73
+ * License:      MIT License
+ *
+ * @package Nebula
+ */
+
+namespace Eighteen73\Nebula;
+
+use Carbon_Fields\Carbon_Fields;
+
+add_action(
+	'after_setup_theme',
+	function() {
+		define( 'Carbon_Fields\URL', home_url( '/vendor/htmlburger/carbon-fields' ) );
+		Carbon_Fields::boot();
+	}
+);
