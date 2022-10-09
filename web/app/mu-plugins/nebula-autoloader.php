51011570403b78faa5c14d2cb18218c5f341c8fb
Ed Jeavons <ed@orphans.co.uk>
2022-10-09 14:16:53 +0100

    Use our new mu-plugins autoloader

--- web/app/mu-plugins/nebula-autoloader.php
+++ web/app/mu-plugins/nebula-autoloader.php
@@ -3,7 +3,7 @@
  * Plugin Name:  Nebula Autoloader
  * Plugin URI:   https://github.com/eighteen73/nebula/
  * Description:  An autoloader that enables standard plugins to be required just like must-use plugins. The autoloaded plugins are included during mu-plugin loading. An asterisk (*) next to the name of the plugin designates the plugins that have been autoloaded.
- * Version:      0.1.0
+ * Version:      1.0.0
  * Author:       eighteen73
  * License:      MIT License
  *
@@ -12,8 +12,8 @@
 
 namespace Eighteen73\Nebula;
 
-use Roots\Bedrock\Autoloader;
+use Eighteen73\NebulaTools\MUPlugins;
 
-if ( is_blog_installed() && class_exists( Autoloader::class ) ) {
-	new Autoloader();
+if ( is_blog_installed() && class_exists( MUPlugins::class ) ) {
+	new MUPlugins();
 }
